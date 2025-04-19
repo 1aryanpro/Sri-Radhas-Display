d76953ab0c3c0425565817535342d55d96d2c6f0
Aryan Prodduturi <aryanpro@icloud.com>
2025-04-19 02:29:52 -0400

    changed carousel logic slightly

--- src/routes/display/+page.svelte
+++ src/routes/display/+page.svelte
@@ -1,6 +1,7 @@
 <script>
     import { supabase } from "$lib/supabase";
     import { onMount } from "svelte";
+    import Carousel from "svelte-carousel/src/components/Carousel/Carousel.svelte";
 
     let images = [];
     let current = 0;
@@ -9,39 +10,81 @@
     const BUCKET_NAME = "carousel";
 
     onMount(async () => {
+        if (updateImages()) startCarousel();
+
+        return () => clearInterval(interval);
+    });
+
+    async function updateImages() {
         const { data, error } = await supabase.storage
             .from(BUCKET_NAME)
             .list("", { limit: 100 });
 
         if (error) {
             console.error("Error fetching images:", error.message);
-        } else {
-            images = data
-                .filter((file) => file.name !== ".emptyFolderPlaceholder")
-                .filter((file) => /\.(jpg|jpeg|png|webp|gif)$/i.test(file.name))
-                .map(
-                    (file) =>
-                        supabase.storage
-                            .from(BUCKET_NAME)
-                            .getPublicUrl(file.name).data.publicUrl,
-                );
-
-            startCarousel();
+            return false;
         }
 
-        return () => clearInterval(interval);
-    });
+        images = data
+            .filter((file) => file.name !== ".emptyFolderPlaceholder")
+            .filter((file) => /\.(jpg|jpeg|png|webp|gif)$/i.test(file.name))
+            .map(
+                (file) =>
+                    supabase.storage.from(BUCKET_NAME).getPublicUrl(file.name)
+                        .data.publicUrl,
+            );
+
+        if (current >= images.length) current = 0;
+        return true;
+    }
+
+    let progress = 0;
+    let progressInterval,
+        progressStep = 1000,
+        resetting = false;
+
+    const imageTime = 30 * 1000;
 
     function startCarousel() {
-        interval = setInterval(() => {
-            current = (current + 1) % images.length;
-        }, 10*1000);
+        updateProgress();
+    }
+
+    function updateProgress() {
+        progressInterval = setInterval(() => {
+            progress += (progressStep / imageTime) * 100;
+            if (progress > 100) {
+                clearInterval(progressInterval);
+                current = (current + 1) % images.length;
+                resetProgress();
+                updateImages();
+            }
+        }, progressStep);
+    }
+
+    function resetProgress() {
+        resetting = true;
+        progress = 0;
+
+        requestAnimationFrame(() => {
+            resetting = false;
+            updateProgress();
+        });
+
+        clearInterval(progressInterval);
     }
 </script>
 
 <div class="page">
     <div class="carousel">
         <img src={images[current]} alt="Carousel" />
+        <div class="progress-bar-container">
+            <div
+                class="progress-bar"
+                class:resetting-bar={resetting}
+                style="width: {progress}%; transition:
+            width {progressStep}ms linear;"
+            ></div>
+        </div>
     </div>
 </div>
 
@@ -62,6 +105,7 @@
 
     .carousel {
         width: 100%;
+        position: relative;
     }
 
     img {
@@ -70,5 +114,20 @@
         box-shadow: 0 4px 20px rgba(0, 0, 0, 0.1);
         transition: opacity 0.5s ease-in-out;
     }
-</style>
 
+    .progress-bar-container {
+        height: 1vh;
+        width: 100%;
+        background: rgba(255, 255, 255, 0.1);
+        overflow: hidden;
+    }
+
+    .progress-bar {
+        height: 1vh;
+        background: #00bfff;
+    }
+
+    .resetting-bar {
+        transition: none !important;
+    }
+</style>
